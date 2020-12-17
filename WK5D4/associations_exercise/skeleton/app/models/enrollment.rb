aa460f9caa283d54ae7d86c54676f3af3b8c5449
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-17 14:53:36 -0800

    Associated User with Enrollment

--- WK5D4/associations_exercise/skeleton/app/models/enrollment.rb
+++ WK5D4/associations_exercise/skeleton/app/models/enrollment.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    belongs_to(
+        :user,
+        foreign_key: :student_id,
+        primary_key: :id,
+        class_name: 'User'
+    )
+
+
+
 end
+
--- WK5D4/associations_exercise/skeleton/app/models/user.rb
+++ WK5D4/associations_exercise/skeleton/app/models/user.rb
@@ -1,2 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    has_many(
+        :enrollments,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: 'Enrollment'
+    )
 end
+
+# class Course < ApplicationRecord
+#   belongs_to(
+#     :professor,
+#     class_name: 'Professor',
+#     foreign_key: :professor_id,
+#     primary_key: :id
+#   )
+# end
+
+# class Professor < ApplicationRecord
+#   has_many(
+#     :courses,
+#     class_name: 'Course',
+#     foreign_key: :professor_id,
+#     primary_key: :id
+#   )
+# end
