5d4cf7b87bd13fe92a20544d6ba29271e28f04e5
kylemuch <kylemuch@gmail.com>
2021-01-04 13:37:15 -0800

    setup 99cats auth

---
+++ WK6D2/first_routes_and_controllers/my_first_routes/db/migrate/20201230234117_add_validations_to_user.rb
@@ -0,0 +1,6 @@
+class AddValidationsToUser < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :name, :string, null: false
+    change_column :users, :email, :string, null: false
+  end
+end
