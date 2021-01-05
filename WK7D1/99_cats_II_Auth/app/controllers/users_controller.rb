544b096906ef9389b20601446f58edcb4287b405
kylemuch <kylemuch@gmail.com>
2021-01-04 23:08:47 -0800

    debugging sign-up

--- WK7D1/99_cats_II_Auth/app/controllers/users_controller.rb
+++ WK7D1/99_cats_II_Auth/app/controllers/users_controller.rb
@@ -5,17 +5,22 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def show
+    @user = User.find_by(params[:id])
+
+    render :show
+  end
+
   def create
     @user = User.new(user_params)  
 
     if @user.save
+      login(@user)
       redirect_to user_url(@user) #we might need to change this part
     else
       render :new
     end
   end
-
-
 
 
   def user_params
--- WK7D1/99_cats_II_Auth/app/models/user.rb
+++ WK7D1/99_cats_II_Auth/app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   end
 
   def password=(password)
-    password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
 
     @password = password
   end
