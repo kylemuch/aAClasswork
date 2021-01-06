cb5c02bebcb696d6d62904d79ffe83a6aeec3928
kylemuch <kylemuch@gmail.com>
2021-01-05 22:49:03 -0800

    first_routes redo

--- WK6D2/first_routes_and_controllers/my_first_routes/db/migrate/20201230234117_add_validations_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddValidationsToUser < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :name, :string, null: false
-    change_column :users, :email, :string, null: false
-  end
-end
