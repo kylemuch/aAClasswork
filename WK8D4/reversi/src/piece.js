5dbea232e5e27a834ea58af5ec20c337612c21ba
kylemuch <kylemuch@gmail.com>
2021-01-14 12:14:04 -0800

    Working on board funcitons

---
+++ WK8D4/reversi/src/piece.js
@@ -0,0 +1,37 @@
+/**
+ * Initializes the Piece with its color.
+ */
+function Piece (color) {
+    this.color = color;
+}
+
+/**
+ * Returns the color opposite the current piece.
+ */
+Piece.prototype.oppColor = function () {
+};
+
+/**
+ * Changes the piece's color to the opposite color.
+ */
+Piece.prototype.flip = function () {
+    if (this.color === 'black') {
+        this.color = 'white';
+    } else {
+        this.color = 'black';
+    }
+};
+
+/**
+ * Returns a string representation of the string
+ * based on its color.
+ */
+Piece.prototype.toString = function () {
+    return this.color[0].toUpperCase();
+};
+
+// DON'T TOUCH THIS CODE
+if (typeof window === 'undefined'){
+    module.exports = Piece;
+}
+// DON'T TOUCH THIS CODE
