4012bcc453defa609fa8638cea6181829d723db2
kylemuch <kylemuch@gmail.com>
2021-01-18 17:24:10 -0800

    bubbleSort complete

--- WK8D3/intro_js_exercises/phase_3_iteration.js
+++ WK8D3/intro_js_exercises/phase_3_iteration.js
@@ -1,16 +1,19 @@
 Array.prototype.bubbleSort = function() {
   //we need to iterate through the array and compare the elements immediately adjacent in the array.
   let arr = this.slice();
+  let sorted = false;
 
-  for (let i = 0; i < arr.length; i += 1) {
-    let el1 = arr[i];
-    let el2 = arr[i + 1];
-    let tempStorage;
-
-    if (el1 > el2) {
-      tempStorage = el1;
-      arr[i] = el2;
-      arr[i + 1] = tempStorage;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < arr.length; i += 1) {
+      let tempStorage;
+  
+      if (arr[i] > arr[i + 1]) {
+        sorted = false;
+        tempStorage = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tempStorage;
+      }
     }
   }
 
